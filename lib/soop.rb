502725c3bfee2520ff5faa49c6d9be3760a59b01
Brent Snook <brent@fuglylogic.com>
2010-02-09 21:00:47 +0000

    allow immediate mixing

--- lib/soop.rb
+++ lib/soop.rb
@@ -5,4 +5,14 @@ module Soop
   
   VERSION = '0.0.0'
   
+  def self.mix target_module, options
+    mixin_modules = options[:with]
+    target_module.constants.each do |constant_name|
+      target = target_module.const_get constant_name
+      mixin_modules.each do |mixin_module|
+        mixin = mixin_module.const_get target.name.split('::')[-1]
+        target.send(:include, mixin)   
+      end  
+    end
+  end
 end
--- spec/soop_spec.rb
+++ spec/soop_spec.rb
@@ -2,4 +2,22 @@ require File.dirname(__FILE__) + '/spec_helper'
 
 describe Soop do
   
+  it 'can mix classes immediately' do
+    
+    module Core
+      class Cat;end
+    end
+    
+    module Presentation
+      module Cat
+        def added_method; end  
+      end
+    end
+    
+    Soop.mix Core, :with => [Presentation]
+    Core::Cat.new.should respond_to :added_method  
+  end
+  
+  it "can mix classes as they're loaded by ActiveSupport"
+  
 end
